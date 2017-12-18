89ce26203d55d83e6607577138d237b840ead300
DLakin01 <DLakin01@gmail.com>
2017-12-18 14:51:07 -0500

    Added get_me method, JSON parsing

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,11 +1,18 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
+  include JSON
 
   def initialize(email, password)
     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
     @auth_token = response["auth_token"]
-    @bio = response["user"]["bio"]
+    #@bio = response["user"]["bio"]
+  end
+
+  def get_me
+    response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token })
+    body = JSON.parse(response.body)
   end
 end
